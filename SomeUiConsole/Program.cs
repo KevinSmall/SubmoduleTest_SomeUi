5e16fc1e838053537d9ef52d8917ed9d00dc5f7c
KevinSmall <kevin_small@hotmail.com>
2019-11-13 10:17:48 +0000

    console hello world

---
+++ SomeUiConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SomeUiConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Task.Delay(100);
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
